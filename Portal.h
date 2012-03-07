7d2c73a2791b6c4dea88a74caff7c6207e5800df
unknown <Sylvon@Sylvon-PC.(none)>
2012-03-06 21:13:23 -0800

    Hello git!

---
+++ Portal.h
@@ -0,0 +1,21 @@
+#ifndef CS248_PORTAL_H
+#define CS248_PORTAL_H
+
+#include "Framework.h"
+
+typedef struct Rectangle PortalDoor;
+
+class Portal{
+public:
+	Portal();
+	~Portal();
+
+private:
+	int neighbors[4];
+	vector<GameObject> objs;
+	vector<PortalDoor> doors;
+	float width;
+	float height;
+}
+
+#endif
