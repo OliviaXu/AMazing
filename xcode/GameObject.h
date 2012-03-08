21d68c82d51b2227dc2638715676cbf00293fc59
Sylvon <Sylvon@Sylvon-PC.(none)>
2012-03-07 22:32:44 -0800

    Initial MapLoader finished. Slight modification of GameObject and Portal header files

--- xcode/GameObject.h
+++ xcode/GameObject.h
@@ -2,17 +2,26 @@
 #define CS248_GAMEOBJECT_H
 
 #include "Framework.h"
+#include "Shader.h"
 
 class GameObject {
 public:
     GameObject();
+	~GameObject();//TODO: Class name is generated via strdup. Free it!
     Vec3 getPos();
     void draw();
-    void updatePhysicalProperty(Vec3 newPos, float newMass, float newVel, float newAcc;);
+    void updatePhysicalProperty(Vec3 newPos, float newMass, float newVel, float newAcc);
+	void setPortal(int iPortal);
+	void setShader(int iShader);
+	void setMass(int mass);
+	void setPos(Vec3 &pos);
+	void setClass(char *className);
+
 private:
     struct Vec3 pos;
     Shader *shader;
     const aiScene *model;
+	char *className;
     int iTex;
     int iPortal;     // the portal this object is in
     float mass;
--- xcode/Portal.h
+++ xcode/Portal.h
@@ -11,12 +11,15 @@ public:
 	~Portal();
     void draw(Camera &camera);    // draw all the objects in it
     int *getNeighbors();
-	void in(struct Vec3 pos);
+	void in(struct Vec3 &pos);
+	void addObject(GameObject *obj);
+	void setPortalObject(GameObject *obj);
 
 private:
 	int neighbors[4];
-	vector<GameObject> objs;
-	vector<PortalDoor> doors;
+	GameObject *portalObj; //In our case, the corridor
+	vector<GameObject *> objs;
+	vector<PortalDoor *> doors;
     struct Vec3 pos;    // SW corner
 	float width;
 	float height;
