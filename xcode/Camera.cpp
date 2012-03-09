dbc67352f63e736d682c901882830d62ad20ff59
Olivia Xu <wenjia.x@hotmail.com>
2012-03-08 21:05:46 -0800

    aaaa....

--- xcode/Camera.cpp
+++ xcode/Camera.cpp
@@ -26,5 +26,14 @@ void Camera::updatePos(struct Vec3 &newBallPos) {
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
+	//computer camera location according to the location of the ball
+
+	//need to be set according to the orientation of the portal and previous moving direction...
+	//get current portal, width>height
+	struct Vec3 camloc(newBallPos.x,newBallPos.y+50,newBallPos.z+50);//about the z+- where...
+	//get the direction according to ...
+	//only can be either (+-1.,0.,0.) or (0.,0.,+-1.)
+	//how did we solve the camera direction? how did we computer...
+	//gluLookAt(camloc.x,camloc.y,camloc.z,dir.x,dir.y,dir.z,0.,1.,0.);
 	gluLookAt(100, 0, 0, 0, 0, 0, 0, 1, 0);
 }
