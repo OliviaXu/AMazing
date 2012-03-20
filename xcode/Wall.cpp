1bf1af80de789274d5e7e1c67ebb9ea8a030b278
Olivia Xu <wenjia.x@hotmail.com>
2012-03-19 20:19:32 -0700

    parallex mapping

--- xcode/Wall.cpp
+++ xcode/Wall.cpp
@@ -30,20 +30,51 @@ void Wall::setTan(const aiMesh *mesh) {
 
 void Wall::setNormalTex(const sf::Image *normalTex){
 	this->normalTex = normalTex;
-}
+	
+	GL_CHECK(glActiveTexture(GL_TEXTURE2));
+	GLuint ntexture;
+	glGenTextures( 1, &ntexture );
+	glBindTexture( GL_TEXTURE_2D,ntexture );
+	//} //always pass in texture even if it is purely blank or black
+	glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );//?need it?
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA,normalTex->GetWidth(),normalTex->GetHeight(),GL_RGBA,GL_UNSIGNED_BYTE,this->normalTex->GetPixelsPtr());
+	
+	}
 
 void Wall::setDepthTex(const sf::Image *depthTex){
 	this->depthTex = depthTex;
+	GL_CHECK(glActiveTexture(GL_TEXTURE3));
+	GLuint dtexture;
+	glGenTextures( 1, &dtexture );
+	glBindTexture( GL_TEXTURE_2D,dtexture);
+	//if(stex)
+		//depthTex->Bind();
+	//else
+	//	default_tex.Bind();
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	gluBuild2DMipmaps(GL_TEXTURE_2D,GL_RGBA,depthTex->GetWidth(),depthTex->GetHeight(),GL_RGBA,GL_UNSIGNED_BYTE,this->depthTex->GetPixelsPtr());
 }
 
 void Wall::setNDTex(){
 	GLuint shaderID = shader->programID();
 	GLint nMap = GL_CHECK(glGetUniformLocation(shaderID, "normalMap"));
+
+	/*
 	GL_CHECK(glActiveTexture(GL_TEXTURE2));
 	GLuint ntexture;
 	glGenTextures( 1, &ntexture );
 	glBindTexture( GL_TEXTURE_2D,ntexture );
-
 	//if(normalTex){
 	//	normalTex->Bind();
 	//}
@@ -58,9 +89,12 @@ void Wall::setNDTex(){
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA,normalTex->GetWidth(),normalTex->GetHeight(),GL_RGBA,GL_UNSIGNED_BYTE,normalTex->GetPixelsPtr());
+	*/
 	GL_CHECK(glUniform1i(nMap, 2)); // The diffuse map will be GL_TEXTURE0
 
 	GLint dMap = GL_CHECK(glGetUniformLocation(shaderID, "depthMap"));
+	
+	/*
 	GL_CHECK(glActiveTexture(GL_TEXTURE3));
 	GLuint dtexture;
 	glGenTextures( 1, &dtexture );
@@ -76,5 +110,6 @@ void Wall::setNDTex(){
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	gluBuild2DMipmaps(GL_TEXTURE_2D,GL_RGBA,depthTex->GetWidth(),depthTex->GetHeight(),GL_RGBA,GL_UNSIGNED_BYTE,depthTex->GetPixelsPtr());
+	*/
 	GL_CHECK(glUniform1i(dMap,3));
 }
